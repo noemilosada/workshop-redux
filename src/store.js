dd867acf521296c68f9fad09e53a6683c4526bb4
Noemi Losada <noemi@mendeley.com>
2016-12-21 12:41:10 +0000

    Add async action to the store

--- src/store.js
+++ src/store.js
@@ -3,5 +3,5 @@ const thunkMiddleware = require('redux-thunk').default;
 const reducer = require('./reducers');
 
 module.exports = function (initialItems) {
-	return redux.createStore(reducer, { items: initialItems }, redux.applyMiddleware(thunkMiddleware));
+  return redux.createStore(reducer, { items: initialItems }, redux.applyMiddleware(thunkMiddleware));
 };
